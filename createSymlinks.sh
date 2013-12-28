a8bd2efaaaece033cc2ff580ebaf953cfad9c1e2
Shaun Mucalo <spm105@uclive.ac.nz>
2013-12-28 20:20:07 +1300

    Removed zsh and pentadactyl config
    clean up createSymlinks.sh to work without these files, includes small
    bugfix missed from previous commit.

--- createSymlinks.sh
+++ createSymlinks.sh
@@ -5,7 +5,7 @@ if [ ! -d ~/.config ]
   then mkdir ~/.config
 fi
 
-declare -a links=(.config/awesome .Xresources .zshrc .ncmpcpp)
+declare -a links=(.config/awesome .Xresources .ncmpcpp)
 
 # If files already exist create backups
 for i in ${links[*]}
@@ -21,11 +21,6 @@ ln -s $HOME/.dotfiles/awesome/ $HOME/.config/awesome
 # X
 ln -s $HOME/.dotfiles/X/xinitrc $HOME/.xinitrc
 ln -s $HOME/.dotfiles/X/Xresources $HOME/.Xresources
-ln -s $HOME/.dotfiles/X/XkeymapUS $HOME/.XkeymapUS
-ln -s $HOME/.dotfiles/X/XkeymapDE $HOME/.XkeymapDE
-
-# zsh
-ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 
 # ncmpcpp
 ln -s $HOME/.dotfiles/ncmpcpp/ $HOME/.ncmpcpp
@@ -33,8 +28,4 @@ ln -s $HOME/.dotfiles/ncmpcpp/ $HOME/.ncmpcpp
 # ranger
 ln -s $HOME/.dotfiles/ranger/ $HOME/.config/ranger
 
-# browser
-ln -s $HOME/.dotfiles/browser/pentadactylrc $HOME/.pentadactylrc
-
-echo "You'll have to symlink the userChrome.css file manually if you want to use it."
 echo "If you want to setup mail, check the .dotfiles/mail directory."
