4a5c033fdabe1e4fd46dd67649d7c44078bfc40d
Shaun Mucalo <spm105@uclive.ac.nz>
2017-12-07 22:32:30 +1300

    Moved stuff around. Not much else

--- emacs/init_general.el
+++ emacs/init_general.el
@@ -1,8 +1,6 @@
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+
 (setq inhibit-startup-screen t)
 (setq mouse-yank-at-point t)
-(setq-default ispell-program-name "aspell")
 (setq ispell-list-command "list")
 (setq ispell-extra-args '("--sug-mode=fast")) ;; --sug-mode=fast,normal
 (setq ispell-local-dictionary "en_GB")
@@ -14,6 +12,25 @@
 (setq kept-old-versions 2)
 (setq version-control t)
 (setq mouse-autoselect-window t)
+(setq show-paren-delay 0)
+(setq save-interprogram-paste-before-kill t)
+(setq auto-revert-verbose nil)
+(setq vc-follow-symlinks t)
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+(setq-default ispell-program-name "aspell")
+(setq-default save-place t)
+(setq-default fill-column 80)
+
+(show-paren-mode 1)
+(global-hl-line-mode t)
+(transient-mark-mode t)
+(global-auto-revert-mode 1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Stop doing bad things
 (define-key global-map [(insert)] nil)
@@ -29,10 +46,7 @@
 (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
   (add-hook hook (lambda () (flyspell-mode -1))))
 
-
-
 (recentf-mode 1)
-(global-set-key (kbd "C-x C-r") 'recentf-open-files)
 (setq recentf-max-saved-items 50)
 (add-to-list 'recentf-exclude "/\\.git/.*\\")         ; ignore git contents
 (add-to-list 'recentf-exclude ".*/elpa/.*\\")           ; package files
@@ -47,55 +61,40 @@
 (add-to-list 'recentf-exclude "/COMMIT_EDITMSG$")
 (recentf-cleanup)
 
-(show-paren-mode 1)
-(setq show-paren-delay 0)
-
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(add-to-list 'auto-mode-alist '("\\.*rc$" . conf-unix-mode))
-
-
-;; Remove line break at end of line
-(global-set-key (kbd "M-j")
-                (lambda ()
-                  (interactive)
-                  (join-line -1)))
-
-
-
-
-;; (defun dot-emacs (relative-path)
-;;       "Return the full path of a file in the user's emacs directory."
-;;       (expand-file-name (concat user-emacs-directory relative-path)))
+(setq completion-ignored-extensions
+      '(".o" ".elc" "~" ".bin" ".class" ".exe" ".ps" ".abs" ".mx"
+        ".~jv" ".rbc" ".pyc" ".beam" ".aux" ".out" ".pdf" ".hbc"))
 
 (setq package-archives '(;; ("gnu" . "http://elpa.gnu.org/packages/")
                          ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("elpy" . "https://jorgenschaefer.github.io/packages/")))
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
 
-;; Goto-line shortcut key
+(add-to-list 'auto-mode-alist '("\\.*rc$" . conf-unix-mode))
+
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-j") (lambda () (interactive) (join-line -1)))
+(global-set-key (kbd "<f5>") 'revert-buffer)
+(global-set-key (kbd "C-x 4") 'toggle-window-split)
+(global-set-key (kbd "C-x 5") 'rotate-windows)
+(global-set-key (kbd "<C-S-down>") 'move-line-down)
+(global-set-key (kbd "<C-S-up>") 'move-line-up)
+(global-set-key (kbd "<C-return>") 'open-line-below)
+(global-set-key (kbd "<C-S-return>") 'open-line-above)
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
 (global-set-key "\M-l" 'goto-line)
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
 
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(setq-default fill-column 80)
-(global-hl-line-mode t)
-
-;; Highlight current region
-(transient-mark-mode t)
-
-;; Add the system clipboard to the kill ring
-(setq save-interprogram-paste-before-kill t)
-
-
-(setq custom-file (dot-emacs "custom.el"))
-(load custom-file 'noerror)
-
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exist. Create it? " dir)))
+                  (make-directory dir t))))))
 
 (use-package lua-mode
   :mode ("\\.lua\\'" . lua-mode))
@@ -120,11 +119,39 @@
   :ensure t
   :disabled t)
 
+;; (use-package auto-complete
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (ac-config-default)
+;;     (global-auto-complete-mode t)
+;;     ))
+
 (use-package company
   :ensure t
+  :diminish ""
   :init
-  (progn
-    (add-hook 'after-init-hook 'global-company-mode)))
+  ;; (add-hook 'prog-mode-hook 'company-mode)
+  ;; (add-hook 'comint-mode-hook 'company-mode)
+  :config
+  (global-company-mode)
+  (setq company-tooltip-limit 10)
+  (setq company-idle-delay 0.2)
+  (setq company-echo-delay 0)
+  (setq company-minimum-prefix-length 3)
+  (setq company-require-match nil)
+  (setq company-selection-wrap-around t)
+  (setq company-tooltip-align-annotations t)
+  (setq company-tooltip-flip-when-above t)
+  (setq company-transformers '(company-sort-by-occurrence)) ; weight by frequency
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+  (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous))
 
 (use-package ido
   :init (progn
@@ -164,49 +191,46 @@
   :config
   (global-set-key (kbd "C-=") 'er/expand-region))
 
-(setq auto-revert-verbose nil) ;; or this
-(global-set-key (kbd "<f5>") 'revert-buffer)
-
                                         ; mark and edit all copies of the marked region simultaniously.
 (use-package iedit
   :ensure t)
 
-                                        ; if you're windened, narrow to the region, if you're narrowed, widen
-                                        ; bound to C-x n
-(defun narrow-or-widen-dwim (p)
-  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
-    Intelligently means: region, org-src-block, org-subtree, or defun,
-    whichever applies first.
-    Narrowing to org-src-block actually calls `org-edit-src-code'.
+(use-package counsel
+:ensure t
+  :bind
+  (("M-y" . counsel-yank-pop)
+   :map ivy-minibuffer-map
+   ("M-y" . ivy-next-line)))
 
-    With prefix P, don't widen, just narrow even if buffer is already
-    narrowed."
-  (interactive "P")
-  (declare (interactive-only))
-  (cond ((and (buffer-narrowed-p) (not p)) (widen))
-        ((region-active-p)
-         (narrow-to-region (region-beginning) (region-end)))
-        ((derived-mode-p 'org-mode)
-         ;; `org-edit-src-code' is not a real narrowing command.
-         ;; Remove this first conditional if you don't want it.
-         (cond ((ignore-errors (org-edit-src-code))
-                (delete-other-windows))
-               ((org-at-block-p)
-                (org-narrow-to-block))
-               (t (org-narrow-to-subtree))))
-        (t (narrow-to-defun))))
+(use-package ivy
+  :ensure t
+  :diminish (ivy-mode)
+  :bind (("C-x b" . ivy-switch-buffer))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "%d/%d ")
+  (setq ivy-display-style 'fancy))
 
-;; (define-key endless/toggle-map "n" #'narrow-or-widen-dwim)
-;; This line actually replaces Emacs' entire narrowing keymap, that's
-;; how much I like this command. Only copy it if that's what you want.
-(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)
+         ("C-c C-r" . ivy-resume)
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
 
 (use-package ace-jump-mode
   :ensure t
   :bind
   ("C-." . ace-jump-mode))
-
-
 
 (use-package smartparens
   :ensure t
@@ -225,12 +249,11 @@
     ("C-M-a" . sp-beginning-of-sexp)
     ("C-M-e" . sp-end-of-sexp)))
 
-
 (use-package simple-mpc
   :ensure t)
+
 (use-package mingus
   :ensure t)
-
 
 (use-package ido-vertical-mode
   :init
@@ -268,12 +291,12 @@
               ;; ("C-down" . comint-next-matching-input-from-input)
               ))
 
-
-;; Remember the last visited line in a file
-(setq-default save-place t)
 (use-package saveplace
   :config
   (setq save-place-file "~/.emacs.d/places"))
+
+
+;; defuns
 (defun toggle-window-split()
   (interactive)
   (if (= (count-windows) 2)
@@ -298,8 +321,6 @@
           (set-window-buffer (next-window) next-win-buffer)
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
-
-(global-set-key (kbd "C-x 4") 'toggle-window-split)
 
 ;; Rotate windows because you opened it in the wrong one
 (defun rotate-windows ()
@@ -327,9 +348,6 @@
              (set-window-start w2 s1)
              (setq i (1+ i)))))))
 
-(global-set-key (kbd "C-x 5") 'rotate-windows)
-
-
 (defun cleanup-buffer-safe ()
   "Perform a bunch of safe operations on the whitespace content of a buffer.
     Does not indent buffer, because it is used for a before-save-hook, and that
@@ -349,8 +367,6 @@
   (cleanup-buffer-safe)
   (indent-region (point-min) (point-max)))
 
-(global-set-key (kbd "C-c n") 'cleanup-buffer)
-
 
 (defun delete-current-buffer-file ()
   "Removes file connected to current buffer and kills buffer."
@@ -365,7 +381,7 @@
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
 
-(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+
 
 (defun open-line-below ()
   (interactive)
@@ -379,9 +395,6 @@
   (newline)
   (forward-line -1)
   (indent-for-tab-command))
-
-(global-set-key (kbd "<C-return>") 'open-line-below)
-(global-set-key (kbd "<C-S-return>") 'open-line-above)
 
 (defun move-line-down ()
   (interactive)
@@ -400,8 +413,6 @@
       (transpose-lines -1))
     (move-to-column col)))
 
-(global-set-key (kbd "<C-S-down>") 'move-line-down)
-(global-set-key (kbd "<C-S-up>") 'move-line-up)
 
 (defun comint-delchar-or-eof-or-kill-buffer (arg)
   (interactive "p")
@@ -414,14 +425,8 @@
             (define-key shell-mode-map
               (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
 
-;; Auto refresh buffers
-(global-auto-revert-mode 1)
 
-;; Also auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
 
-(global-set-key [remap goto-line] 'goto-line-with-feedback)
 
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
@@ -432,47 +437,36 @@
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
 
-(use-package counsel
-:ensure t
-  :bind
-  (("M-y" . counsel-yank-pop)
-   :map ivy-minibuffer-map
-   ("M-y" . ivy-next-line)))
+                                        ; if you're windened, narrow to the region, if you're narrowed, widen
+                                        ; bound to C-x n
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+    Intelligently means: region, org-src-block, org-subtree, or defun,
+    whichever applies first.
+    Narrowing to org-src-block actually calls `org-edit-src-code'.
 
-(use-package ivy
-  :ensure t
-  :diminish (ivy-mode)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "%d/%d ")
-  (setq ivy-display-style 'fancy))
+    With prefix P, don't widen, just narrow even if buffer is already
+    narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing command.
+         ;; Remove this first conditional if you don't want it.
+         (cond ((ignore-errors (org-edit-src-code))
+                (delete-other-windows))
+               ((org-at-block-p)
+                (org-narrow-to-block))
+               (t (org-narrow-to-subtree))))
+        (t (narrow-to-defun))))
 
-
-(use-package swiper
-  :ensure t
-  :bind (("C-s" . swiper)
-         ("C-r" . swiper)
-         ("C-c C-r" . ivy-resume)
-         ("M-x" . counsel-M-x)
-         ("C-x C-f" . counsel-find-file))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq ivy-display-style 'fancy)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
+;; (define-key endless/toggle-map "n" #'narrow-or-widen-dwim)
+;; This line actually replaces Emacs' entire narrowing keymap, that's
+;; how much I like this command. Only copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
 
 ;; (use-package avy
 ;; :ensure t
 ;; :bind ("M-s" . avy-goto-word-1)) ;; changed from char as per jcs
-
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
