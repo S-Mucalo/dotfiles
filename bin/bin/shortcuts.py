b1401e84786671c34326e7947762018c066e9fe5
Shaun Mucalo <spm105@uclive.ac.nz>
2018-11-07 11:17:11 +1300

    fixed up alias shortcuts

--- bin/bin/shortcuts.py
+++ bin/bin/shortcuts.py
@@ -6,66 +6,71 @@ import sys
 from re import sub
 from re import compile
 
+import os
+import pwd
+username = pwd.getpwuid(os.getuid())[0]
+
 folders = (("h", "~"),
            ("d", "~/Documents"),
            ("D", "~/Downloads"),
            ("pp", "~/Pictures"),
+           ("N", "~/Nextcloud"),
            ("vv", "~/Videos"),
            ("m", "~/Music"),
            ("b", "~/Books"),
-           ("c", "~/.dotfiles/"),
+           ("c", "~/dotfiles/"),
            ("r", "/"),
+           ("M", "/run/media/{}".format(username)),
            ("cf", "~/.config"))
 
 configs = (("cfz", "~/.zshrc"),
-           ("cfe", "~/.emacs.d/init.el"),
+           ("cfe", "~/.emacs.d/config.org"),
            ("cfr", "~/.config/ranger/rc.conf"),
            ("cfq", "~/.config/qutebrowser/config.py"),
-           ("cft", "~/.tmux.conf/"),
+           ("cft", "~/.tmux.conf"),
            ("cfd", "~/.Xresources"),
            ("cfi", "~/.config/i3/config"))
-
-
 
 quteshortcuts = ""
 rangershortcuts = ""
 zshshortcuts = ""
-home = str(Path.home())+"/"
-rangerlocation=home+".config/ranger/rc.conf"
-zshlocation=home+".config/zsh/aliases_zsh"
-qutelocation=home+".config/qutebrowser/config.py"
+home = Path.home()
+
+rangerlocation = Path.joinpath(home, ".config/ranger/rc.conf")
+zshlocation = Path.joinpath(home, ".config/zsh/01-aliases.zsh")
+qutelocation = Path.joinpath(home, ".config/qutebrowser/config.py")
 
 
 # These are the labels that demarcate where the shortcuts
 # go in the config files.
-beg="# SHORTCUT START\n"
-end="# SHORTCUT END"
+beg = "# SHORTCUT START\n"
+end = "# SHORTCUT END"
 
 #First we open the list of folder shortcuts and go down each line adding each in the required syntax to each of the three configs:
 
 # with open(home+".config/Scripts/folders") as fold:
 #     for line in csv.reader(fold, dialect="excel-tab"):
 for fold in folders:
-    #Adds the ranger go, tab, move and yank commands:
-    rangershortcuts+=("map g"+fold[0]+" cd "+fold[1]+"\n")
-    rangershortcuts+=("map t"+fold[0]+" tab_new "+fold[1]+"\n")
-    rangershortcuts+=("map m"+fold[0]+" shell mv %s "+fold[1]+"\n")
-    rangershortcuts+=("map Y"+fold[0]+" shell cp -r %s "+fold[1]+"\n")
-    #Adds the bashshortcuts shortcuts:
-    zshshortcuts+=("alias "+fold[0]+"=\"cd "+fold[1]+" && ls -a\"\n")
-    #qutebrowser shortcuts:
-    quteshortcuts+="config.bind(';"+fold[0]+"', 'set downloads.location.directory "+fold[1]+" ;; hint links download')\n"
+    # Adds the ranger go, tab, move and yank commands:
+    rangershortcuts += ('map g{} cd {}\\n'.format(fold[0], fold[1]))
+    rangershortcuts += ('map t{} tab_new {}\\n'.format(fold[0], fold[1]))
+    rangershortcuts += ('map m{} shell mv %s {}\\n'.format(fold[0], fold[1]))
+    rangershortcuts += ('map Y{} shell cp -r %s {}\n'.format(fold[0], fold[1]))
+    # Adds the bashshortcuts shortcuts:
+    zshshortcuts += ('alias {}="cd {} && ls -a"\n'.format(fold[0], fold[1]))
+    # qutebrowser shortcuts:
+    quteshortcuts += ('config.bind(";{}", "set downloads.location.directory {} ;; hint links download"\n')
 
-#Goes thru the config file file and adds the shortcuts to both zshshortcuts and ranger.
+# Goes thru the config file file and adds the shortcuts to both zshshortcuts and ranger.
 
 for conf in configs:
-    zshshortcuts+=("alias "+conf[0]+"=\"emacsclient -nc -a \\\"\\\" "+conf[1]+"\"\n")
-    rangershortcuts+=("map "+conf[0]+" shell emacsclient -nc -a \"\" "+conf[1]+"\n")
+    zshshortcuts += ('alias {}="emacsclient -nc -a emacs {}"\n'.format(conf[0], conf[1]))
+    rangershortcuts += ('map {} shell emacsclient -nc -a emacs {}\n'.format(conf[0], conf[1]))
 
 
 def replaceInMarkers(text, shortcuts):
-    markers = compile(beg+"(.|\s)*"+end)
-    replacement =beg+shortcuts+end
+    markers = compile(beg + "(.|\s)*" + end)
+    replacement = beg + shortcuts + end
     return sub(markers, replacement, text)
 
 
@@ -82,7 +87,7 @@ def writeShortcuts(location, shortcuts):
 def delShortcuts(location):
     with open(location, "r+") as input:
         markers = compile(beg+"(.|\s)*"+end)
-        replacement =beg + end
+        replacement = beg + end
         final = input.read()
         final = sub(markers, replacement, final)
         input.seek(0)
