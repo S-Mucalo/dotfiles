6947c8649bd4b419ab1911d58e829e334116e253
Shaun Mucalo <spm105@uclive.ac.nz>
2019-06-11 15:28:19 +1200

    Added keepass

--- bin/bin/gkey_passwords.py
+++ bin/bin/gkey_passwords.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python2
-
+from __future__ import print_function
 import gnomekeyring as gkey
 
 def set_credentials(repo, user, pw):
     KEYRING_NAME = "offlineimap"
-    attrs = { "repo": repo, "user": user }
+    attrs = {"repo": repo, "user": user}
     keyring = gkey.get_default_keyring_sync()
     gkey.item_create_sync(keyring, gkey.ITEM_NETWORK_PASSWORD, KEYRING_NAME, attrs, pw, True)
 
 def get_credentials(repo):
     keyring = gkey.get_default_keyring_sync()
-    attrs = { "repo": repo}
+    attrs = {"repo": repo}
     items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
     return (items[0].attributes["user"], items[0].secret)
 
@@ -26,13 +26,12 @@ if __name__ == '__main__':
     import os
     import getpass
     if len(sys.argv) != 3:
-        print "Usage: %s <repository> <username>" \
-            % (os.path.basename(sys.argv[0]))
+        print("Usage: %s <repository> <username>" % (os.path.basename(sys.argv[0])))
         sys.exit(0)
     repo, username = sys.argv[1:]
     password = getpass.getpass("Enter password for user '%s': " % username)
     password_confirmation = getpass.getpass("Confirm password: ")
     if password != password_confirmation:
-        print "Error: password confirmation does not match"
+        print("Error: password confirmation does not match")
         sys.exit(1)
     set_credentials(repo, username, password)
