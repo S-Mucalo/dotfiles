b657b479881eb2470cf0d4b5e9d2cc6a7fe83874
Shaun Mucalo <spm105@uclive.ac.nz>
2020-05-09 22:32:07 +1200

    Add lots of defer to emacs init, likely broken packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,15 @@
-(require 'org)
-(setq vc-follow-symlinks t)                  ; Just do it
-(org-babel-load-file (expand-file-name "settings.org"
-                    user-emacs-directory))
+(let ((file-name-handler-alist nil))
+  ;; temporarily increase garbage collection threshold and turn off file name handler regexp
+  (setq gc-cons-threshold-original gc-cons-threshold)
+  (setq gc-cons-threshold (* 1024 1024 100))
+  (require 'org)
+  (setq vc-follow-symlinks t)                  ; Just do it
+  (org-babel-load-file (expand-file-name "settings.org"
+                                         user-emacs-directory))
+
+  (run-with-idle-timer
+   5 nil
+   (lambda ()
+     (setq gc-cons-threshold gc-cons-threshold-original)
+     (makunbound 'gc-cons-threshold-original)))
+  )
